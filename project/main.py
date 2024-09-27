4ce07de6c1c1fa87af4fb2b4cef2661a5a1da31e
CodeByJamie <jamie.macdonald@nairnacademy.net>
2024-09-27 11:06:50 +0100

    Reading & Writing to files

--- project/main.py
+++ project/main.py
@@ -1,68 +1,26 @@
-values = []
+def readCountries():
+    # Open the file to read it
+    with open('project/countries.txt', 'r') as countriesFile:
+        countries = countriesFile.readlines()  
+        countries = [country.strip() for country in countries]
+        
+    return countries
 
-def readFile():
-    # Open the file for reading
-    with open("./mammals.txt", "r") as filePath:
-        # Read in the data one line at a time
-        lines = filePath.readlines()
 
-        # Loop through the mammals, one at a time
-        for line in lines:
-            mammals = line.strip() 
-            parts = mammals.split(",")
+def findShortest(countries):
+    min = len(countries[0])
 
-            # Save the data
-            town = parts[0]
-            mammal = parts[1]
-            date = parts[2]
-            age = parts[3]
+    for i in range(0, len(countries)):
+        if (len(countries[i]) < min):
+            return countries[i]
+            break
 
-            # Add the values to the array
-            values.append((town, mammal, date, age))
+def writeFile(countries):
+    with open('project/results.txt', 'w') as newCountriesFile:
+        for country in countries:
+            if country.startswith('E'):
+                newCountriesFile.write(country + '\n')
 
-def findOldestMammal():
-    # Set the max_age to -1 for linear search
-    max_age = -1
-    oldest_mammal = None
-
-    # Loop through each entry / line stored in the array
-    for entry in values:
-        town, mammal, date, age = entry
-        try:
-            # Convert age (str) -> age (int)
-            age = int(age)
-
-            # Check if the current age recorded is > max_age so far
-            if age > max_age:
-                # Set the new max with the current age
-                max_age = age
-                oldest_mammal = (town, mammal, date, age)
-
-        except ValueError:
-            print("Invalid age", age, "for mammal", mammal, "in", town)
-    
-    # Check if oldest_mammal is a truthy value
-    if oldest_mammal:
-        town, mammal, date, age = oldest_mammal
-        print("The oldest mammal is a", mammal, "from", town, "seen on", date, "with an age of:", age)
-    else:
-        print("No valid entries found.")
-
-def displaySighting(mammal_name, town_name):
-    # Find and display the dates of sightings of a chosen mammal in a particular town
-    sightings = [(town, mammal, date) for town, mammal, date in values if mammal == mammal_name and town == town_name]
-    
-    if sightings:
-        print("Sightings of", mammal_name, "in", town_name, ":")
-        for sighting in sightings:
-            print(sighting[2])  
-    else:
-        print("No sightings of", mammal_name, "found in", town_name)
-
-# Read the data from the file
-readFile()
-
-# Find and display the age of the oldest mammal
-findOldestMammal()
-
-displaySighting("Squirrel", "Culross")
+countries = readCountries()
+shortestCountry = findShortest(countries)
+writeFile(countries)
